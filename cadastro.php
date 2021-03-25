2e970ac82e41927782455191b61d1049a433ba2d
Allerson <allerson.santana354@gmail.com>
2021-03-25 19:42:07 -0300

    Create cadastro.php
    Página de cadastro de usuário

---
+++ cadastro.php
@@ -0,0 +1,123 @@
+<?php
+require_once('componentes/erro_cadastro.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+
+    <style>
+        body {
+            background-image: url(imgs/pattern3.png);
+        }
+
+        .cadastrar {
+            width: 55%;
+            margin: auto;
+            height: fit-content;
+            margin-top: 2%;
+            font-family: Verdana;
+            background-color: whitesmoke;
+            padding: 2%;
+            border-radius: 5%;
+        }
+
+        .login-1 {
+            padding-bottom: 5%;
+        }
+
+        #row-1 {
+            padding-bottom: 5%;
+        }
+
+        .sexo {
+            color: rgb(117, 117, 117);
+        }
+
+        #btn-voltar {
+            padding: 1%;
+        }
+
+        #senha-1 {
+            padding-bottom: 5%;
+        }
+    </style>
+</head>
+
+<body>
+
+    <body>
+        <form action="index.php" method="$_POST">
+            <div id="btn-voltar" class="btn-group" role="group" aria-label="Basic outlined example">
+                <button type="submit" class="btn btn-dark">Login</button>
+            </div>
+        </form>
+        <div class="cadastrar">
+            <center>
+                <h4>Cadastro</h4>
+            </center>
+            <?php
+            if (!empty($erros)) :
+                foreach ($erros as $erro) :
+                    echo $erro;
+                endforeach;
+            endif;
+            ?>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                <div class="login-1">
+                    <input type="text" name="nome" id="nome" placeholder="Nome" style="width: 100%;">
+                </div>
+                <div class="login-1">
+                    <input type="text" name="snome" id="snome" placeholder="Sobrenome" style="width: 100%;">
+                </div>
+                <div class="row" id="row-1">
+                    <div class="col">
+                        <input type="text" name="email" id="email" placeholder="E-mail" style="width: 100%;">
+                    </div>
+                    <div class="col">
+                        <input type="tel" name="telefone" id="telefone" placeholder="Tel. Ex: (00) 00000-0000" style="width: 100%;">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col" id="senha-1">
+                        <input type="password" name="senha" id="senha" placeholder="Crie uma senha" style="width: 100%;">
+                    </div>
+                    <div class="col" id="senha-1">
+                        <input type="password" name="senha-2" id="senha-2" placeholder="Digite a senha novamente" style="width: 100%;">
+                    </div>
+                    <div class="row" id="row-1">
+                        <div class="col-sm-3">
+                            <input class="form-check-input" type="radio" name="sexo" id="flexRadioDefault1" checked>
+                            <label class="form-check-label" for="flexRadioDefault1" value="Masculino">
+                                Masculino
+                            </label>
+                        </div>
+                        <div class="col">
+                            <input class="form-check-input" type="radio" name="sexo" id="flexRadioDefault2">
+                            <label class="form-check-label" for="flexRadioDefault2" value="Feminino">
+                                Feminino
+                            </label>
+                        </div>
+                        <div class="col-xl-6">
+                            <input type="checkbox" name="termos" id="termos"> Li e concordo com os <a href="">termos</a> de uso
+                        </div>
+                    </div>
+                </div>
+
+                <div class="botao">
+                    <button type="submit" class="btn btn-primary" style="width: 100%;" name="btn-enviar">Registrar</button>
+                </div>
+        </div>
+        </form>
+        </div>
+    </body>
+
+</html>
