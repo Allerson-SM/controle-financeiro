b7337f7077a21cfa8cb80d1e927ba86218f8a5f7
Allerson <allerson.santana354@gmail.com>
2021-03-27 18:03:58 -0300

    BD

---
+++ BD/controle-financeiro.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 27-Mar-2021 às 22:02
+-- Versão do servidor: 10.1.38-MariaDB
+-- versão do PHP: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `controle-financeiro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `lancamentos`
+--
+
+CREATE TABLE `lancamentos` (
+  `ID` int(255) NOT NULL,
+  `DATA` date NOT NULL,
+  `DESCRICAO` varchar(50) NOT NULL,
+  `RECEITA` decimal(20,2) NOT NULL,
+  `DESPESA` decimal(20,2) NOT NULL,
+  `HORARIO` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID` int(255) NOT NULL,
+  `NOME` varchar(30) NOT NULL,
+  `SOBRENOME` varchar(30) NOT NULL,
+  `EMAIL` varchar(50) NOT NULL,
+  `TELEFONE` varchar(20) NOT NULL,
+  `SENHA` varchar(32) NOT NULL,
+  `SEXO` char(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `lancamentos`
+--
+ALTER TABLE `lancamentos`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `lancamentos`
+--
+ALTER TABLE `lancamentos`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
